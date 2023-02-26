c8f43bacbeff46b67a4c9fe990eb1a962911d8cf
Allison <allimacq@gmail.com>
2023-02-26 15:02:33 -0500

    add playRound(player, computer) function

--- game.js
+++ game.js
@@ -13,3 +13,38 @@ const playerSelection = () => {
         playerSelection();
     }
 }
+
+const playRound = (player, computer) => {
+    player = playerSelection();
+    computer = getComputerChoice();
+
+    switch (player) {
+        case "rock":
+            if (computer === "rock") {
+                return "It's a tie!"
+            } else if (computer === "paper") {
+                return "You Lose! Paper beats Rock."
+            } else {
+                return "You Win! Rock beats Scissors."
+            }
+            break;
+        case "paper":
+            if (computer === "rock") {
+                return "You Win! Paper beats Rock."
+            } else if (computer === "paper") {
+                return "It's a tie!"
+            } else {
+                return "You Lose! Scissors beat Paper."
+            }
+            break;
+        case "scissors":
+            if (computer === "rock") {
+                return "You Lose! Rock beats Scissors."
+            } else if (computer === "paper") {
+                return "You Win! Scissors beat Paper."
+            } else {
+                return "It's a tie!"
+            }
+            break;
+    }
+}
