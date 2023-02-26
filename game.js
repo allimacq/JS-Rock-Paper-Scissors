1e62600e891f74dd5ac2bb6bd56b89e6e0e00d81
Allison <allimacq@gmail.com>
2023-02-26 14:25:16 -0500

    add recursion to playerSelection() to handle wrong inputs

--- game.js
+++ game.js
@@ -5,9 +5,11 @@ const getComputerChoice = () => {
 }
 
 const playerSelection = () => {
-        let selection = prompt("Choose Rock, Paper, or Scissors").toLowerCase().trim();
-        if (selection !== "rock" || selection !== "paper" || selection !== "scissors") {
-            selection = prompt("You can only enter Rock, Paper, or Scissors. If you don't, I'll choose Rock.", "rock").toLowerCase().trim();
-        }
+    let selection = prompt("Choose Rock, Paper, or Scissors").toLowerCase().trim();
+    console.log(selection)
+    if (selection === "rock" || selection === "paper" || selection === "scissors") {
         return selection;
+    } else {
+        playerSelection();
+    }
 }
