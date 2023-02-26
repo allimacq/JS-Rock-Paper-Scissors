fc31fe946d0c2683321b5366499f0d85ce38a7a7
Allison <allimacq@gmail.com>
2023-02-26 14:11:38 -0500

    removed playerSelection from getComputerChoice

--- game.js
+++ game.js
@@ -2,12 +2,12 @@ const getComputerChoice = () => {
     const choices = ["rock", "paper", "scissors"];
     let choice = choices[Math.floor(Math.random() * choices.length)];
     return choice;
+}
 
-    const playerSelection = () => {
+const playerSelection = () => {
         let selection = prompt("Choose Rock, Paper, or Scissors").toLowerCase().trim();
         if (selection !== "rock" || selection !== "paper" || selection !== "scissors") {
             selection = propmt("You can only enter Rock, Paper, or Scissors. If you don't, I'll choose Rock.", "rock").toLowerCase().trim();
         }
         return selection;
-    }
 }
